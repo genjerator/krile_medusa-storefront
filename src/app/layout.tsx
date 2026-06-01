2dfb2e8c8953462855993c69d36dc8db6a8b9d2b
Genjerator <e.medjesi@gmail.com>
2026-06-01 16:11:20 +0200

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
 import { NextIntlClientProvider } from "next-intl"
 import { getLocale, getMessages } from "next-intl/server"
+import Script from "next/script"
 import "styles/globals.css"
 
 export const metadata: Metadata = {
@@ -15,6 +16,18 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang={locale} data-mode="light">
       <body cz-shortcut-listen="true">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-CN1PX0WWDT"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CN1PX0WWDT');
+          `}
+        </Script>
         <NextIntlClientProvider messages={messages}>
           <main className="relative">{props.children}</main>
         </NextIntlClientProvider>
