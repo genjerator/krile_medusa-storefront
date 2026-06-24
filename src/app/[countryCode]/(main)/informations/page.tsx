97484a5c99737f6e7867ea0806e2571d6a6979e6
Genjerator <e.medjesi@gmail.com>
2026-06-24 16:10:07 +0200

    informations

--- src/app/[countryCode]/(main)/informations/page.tsx
+++ src/app/[countryCode]/(main)/informations/page.tsx
@@ -17,10 +17,10 @@ export default function InformationsPage() {
         <h3 className="font-heading font-semibold uppercase tracking-widest text-xs text-ui-fg-base mb-3">Bank details</h3>
         <dl className="grid grid-cols-[max-content_1fr] gap-x-8 gap-y-2 text-sm">
           <dt className="text-ui-fg-muted">Owner</dt>
-          <dd className="text-ui-fg-base">Planeta GmbH &amp; Co. KG</dd>
+          <dd className="text-ui-fg-base">Planeta GmbH &amp; Co.KG</dd>
 
           <dt className="text-ui-fg-muted">Institute</dt>
-          <dd className="text-ui-fg-base">Savings Bank MM-LI-MN</dd>
+          <dd className="text-ui-fg-base">Sparkasse Schwaben Bodensee MM-LI-MN</dd>
 
           <dt className="text-ui-fg-muted">BIC / SWIFT</dt>
           <dd className="text-ui-fg-base font-sans">BYLADEM1MLM</dd>
